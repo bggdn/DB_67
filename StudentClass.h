d190296afd99b79dd60431908613f91cc72ecd95
bggdn <128053843+bggdn@users.noreply.github.com>
2023-05-05 19:41:36 +0300

    fix

--- StudentClass.h
+++ StudentClass.h
@@ -419,7 +419,7 @@ public:
 					sum = sum + sn->examsRecordsData[i][j].mark;
 					count++;
 				}
-		int avr = 0;
+		double avr = 0;
 		if (count > 0)
 			avr=sum / count;
 		return avr;
@@ -434,7 +434,7 @@ public:
 					sum = sum + sn->examsRecordsData[i-1][j].mark;
 					count++;
 				}
-		int avr = 0;
+		double avr = 0;
 		if (count > 0)
 			avr = sum / count;
 		return avr;
